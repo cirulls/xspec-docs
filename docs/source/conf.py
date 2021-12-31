0406425e4e02d6679a1d8fea105dfd95057467c6
Sandro Cirulli <sandro@scalefactory.com>
2021-12-31 16:24:03 +0000

    Initial commit with rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'XSpec'
+copyright = '2021-2022, XSpec'
+author = 'cirulls'
 
 release = '0.1'
 version = '0.1.0'
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "myst_parser",
 ]
 
 intersphinx_mapping = {
